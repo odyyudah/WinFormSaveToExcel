3a008d91550e04e8f28ac17715943d4e9709ab59
odyyudah <odyyudah@gmail.com>
2017-06-13 16:07:29 +0700

    initial commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource5 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.NorthwindBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dsNorthwind = new WindowsFormsApplication1.dsNorthwind();
+            this.NorthwindTableAdapter = new WindowsFormsApplication1.dsNorthwindTableAdapters.NorthwindTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NorthwindBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsNorthwind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource5.Name = "DataSet1";
+            reportDataSource5.Value = this.NorthwindBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource5);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WindowsFormsApplication1.Northwind.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(13, 41);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(578, 306);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // NorthwindBindingSource
+            // 
+            this.NorthwindBindingSource.DataMember = "Northwind";
+            this.NorthwindBindingSource.DataSource = this.dsNorthwind;
+            // 
+            // dsNorthwind
+            // 
+            this.dsNorthwind.DataSetName = "dsNorthwind";
+            this.dsNorthwind.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // NorthwindTableAdapter
+            // 
+            this.NorthwindTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save to MSExcel format";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 359);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NorthwindBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsNorthwind)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource NorthwindBindingSource;
+        private dsNorthwind dsNorthwind;
+        private dsNorthwindTableAdapters.NorthwindTableAdapter NorthwindTableAdapter;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.NorthwindTableAdapter.Fill(this.dsNorthwind.Northwind);
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Warning[] warnings;
+            string[] streamids;
+            string mimeType;
+            string encoding;
+            string extension;
+            string filename;
+
+            byte[] bytes = reportViewer1.LocalReport.Render(
+               "EXCELOPENXML", null, out mimeType, out encoding,
+                out extension,
+               out streamids, out warnings);
+
+            filename = string.Format("{0}.{1}", "Northwind", "xlsx");
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Excel 2013|*.xlsx";
+            saveFileDialog1.Title = "Save an Excel File";
+            saveFileDialog1.FileName = filename;
+            saveFileDialog1.ShowDialog();
+            FileStream fs = (FileStream)saveFileDialog1.OpenFile();
+            fs.Write(bytes, 0, bytes.Length);
+            fs.Close();
+        }
+
+        
+    }
+}
